e5cf6b621a8118128082248ddb431bf8b783df02
kochmarvin <marvin.info@gmx.at>
2024-04-11 23:41:33 +0200

    :zap: add options for init command

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -2,14 +2,30 @@ import * as fs from 'fs';
 
 import { Color } from '@utils/color';
 import { JulyUiConfig } from '@commons/july-ui-config';
+import yargs from 'yargs';
+
+const options = (
+	yarg: yargs.Argv<Record<string, unknown>>
+): yargs.Argv<Record<string, unknown>> => {
+	return yargs.positional('path', {
+		describe: 'Path where components will be stored',
+		type: 'string',
+		demandOption: true,
+	});
+};
 
 const handler = (argv: Record<string, unknown>): void => {
 	if (!fs.existsSync('angular.json'))
 		throw new Error(
-			'Error: This command is not available when running the Angular CLI outside a workspace.'
+			'ERROR This command is not available when running the July CLI outside an Angular workspace.'
 		);
 
-	const config: JulyUiConfig = { path: argv.path as string };
+	let path = argv.path as string;
+	if (path && path.startsWith('/')) {
+		path = path.substring(1);
+	}
+
+	const config: JulyUiConfig = { path };
 	fs.writeFileSync('july-ui.config.json', JSON.stringify(config, null, 2));
 	const stats = fs.statSync('july-ui.config.json');
 
@@ -24,7 +40,7 @@ const handler = (argv: Record<string, unknown>): void => {
 const InitCommand = {
 	command: 'init <path>',
 	description: 'Sets the download path for the components',
-	options: () => {},
+	options,
 	handler,
 };
 
